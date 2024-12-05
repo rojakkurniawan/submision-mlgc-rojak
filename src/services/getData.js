8078a34ddd448b2fb8be4a6db707816b87ad6a88
Rojak Kurniawan <rojakkurniawan4@gmail.com>
2024-12-05 19:47:52 +0700

    fix name collection

--- src/services/getData.js
+++ src/services/getData.js
@@ -3,7 +3,7 @@ const { Firestore } = require("@google-cloud/firestore");
 async function getData() {
   const db = new Firestore();
 
-  const predictCollection = db.collection("prediction");
+  const predictCollection = db.collection("predictions");
   const history = await predictCollection.get();
 
   if (history.empty) {
